7c32d9a6f40e098d89928619eeacd9345d612ca0
Shuffled720 <shuffled720@gmail.com>
2024-01-27 12:32:39 +0530

    material added and updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import { FullWeekTT } from "@/components/FullWeekTT"
 import Header from "@/components/Header"
+import Material from "@/components/Material"
 import Menu from "@/components/Menu"
 
 export default function Home() {
   return (
     <>
       <Header />
+
       <FullWeekTT />
+
       <Menu />
+
+      <Material />
     </>
   )
 }

bfc45a79be4150c63a63c62eb81ec3700801486c
Shuffled720 <shuffled720@gmail.com>
2024-09-29 11:04:20 +0530

    skaleton created

---
+++ src/app/toss-coin/page.tsx
@@ -0,0 +1,13 @@
+
+const page = () => {
+
+    return (
+        <div>
+            <h1>Toss Coin</h1>
+            <p>Click the button to toss a coin</p>
+            <button>Toss Coin</button>
+        </div>
+    )
+}
+
+export default page
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,15 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <Link href="https://docs.google.com/spreadsheets/d/1E3ocQqnudZKi6ozj8d4fIpUWeCCveX5_iKzG2fDJafQ/edit?usp=sharing">
-      <h1 className="text-center text-3xl py-2 ">EE Time-Table</h1>
-    </Link>
+    <>
+      <Link href="/">
+        <h1 className="text-center text-3xl py-2 ">EE Time-Table</h1>
+      </Link>
+      <Link href="/toss-coin">
+        <h1 className="text-center text-3xl py-2 ">Toss a coin</h1>
+      </Link>
+
+    </>
   )
 }
 export default Header
